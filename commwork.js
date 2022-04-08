f34abd8b2374e92eec827842a4b8e5a4558d91c7
beso <zarnadzebeso6@gmail.com>
2022-04-08 18:52:47 +0400

    init

---
+++ commwork.js
@@ -0,0 +1,329 @@
+const data = [
+    {
+      id: 1,
+      firstname: "Molly",
+      lastname: "Begbie",
+      email: "mbegbie0@parallels.com",
+      sex: "Female",
+      age: 74,
+      job: "Tax Accountant",
+    },
+    {
+      id: 2,
+      firstname: "Mayer",
+      lastname: "Kuhnke",
+      email: "mkuhnke1@myspace.com",
+      sex: "Male",
+      age: 39,
+      job: "Legal Assistant",
+    },
+    {
+      id: 3,
+      firstname: "Susy",
+      lastname: "Kyneton",
+      email: "skyneton2@buzzfeed.com",
+      sex: "Male",
+      age: 36,
+      job: "Director of Sales",
+    },
+    {
+      id: 4,
+      firstname: "Raquela",
+      lastname: "Littlefield",
+      email: "rlittlefield3@google.co.uk",
+      sex: "Male",
+      age: 69,
+      job: "Business Systems Development Analyst",
+    },
+    {
+      id: 5,
+      firstname: "Robinetta",
+      lastname: "Loughney",
+      email: "rloughney4@typepad.com",
+      sex: "Female",
+      age: 40,
+      job: "Financial Advisor",
+    },
+    {
+      id: 6,
+      firstname: "Orelle",
+      lastname: "Domanski",
+      email: "odomanski5@angelfire.com",
+      sex: "Male",
+      age: 32,
+      job: "VP Accounting",
+    },
+    {
+      id: 7,
+      firstname: "Brandi",
+      lastname: "Haliburton",
+      email: "bhaliburton6@baidu.com",
+      sex: "Male",
+      age: 57,
+      job: "Engineer IV",
+    },
+    {
+      id: 8,
+      firstname: "Ario",
+      lastname: "Cormack",
+      email: "acormack7@sfgate.com",
+      sex: "Male",
+      age: 43,
+      job: "Speech Pathologist",
+    },
+    {
+      id: 9,
+      firstname: "Kirsti",
+      lastname: "Kimmerling",
+      email: "kkimmerling8@statcounter.com",
+      sex: "Female",
+      age: 27,
+      job: "Design Engineer",
+    },
+    {
+      id: 10,
+      firstname: "Orton",
+      lastname: "Renachowski",
+      email: "orenachowski9@amazon.co.uk",
+      sex: "Female",
+      age: 55,
+      job: "Technical Writer",
+    },
+    {
+      id: 11,
+      firstname: "Myca",
+      lastname: "Blinkhorn",
+      email: "mblinkhorna@geocities.jp",
+      sex: "Male",
+      age: 42,
+      job: "Recruiter",
+    },
+    {
+      id: 12,
+      firstname: "Willdon",
+      lastname: "Jennaway",
+      email: "wjennawayb@wunderground.com",
+      sex: "Female",
+      age: 35,
+      job: "Design Engineer",
+    },
+    {
+      id: 13,
+      firstname: "Clarinda",
+      lastname: "Cockley",
+      email: "ccockleyc@plala.or.jp",
+      sex: "Male",
+      age: 72,
+      job: "Design Engineer",
+    },
+    {
+      id: 14,
+      firstname: "Alister",
+      lastname: "Grayham",
+      email: "agrayhamd@technorati.com",
+      sex: "Female",
+      age: 24,
+      job: "Speech Pathologist",
+    },
+    {
+      id: 15,
+      firstname: "Gloria",
+      lastname: "Nell",
+      email: "gnelle@parallels.com",
+      sex: "Female",
+      age: 58,
+      job: "Senior Cost Accountant",
+    },
+    {
+      id: 16,
+      firstname: "Lanette",
+      lastname: "Jendas",
+      email: "ljendasf@cdc.gov",
+      sex: "Male",
+      age: 49,
+      job: "Quality Control Specialist",
+    },
+    {
+      id: 17,
+      firstname: "Emmalee",
+      lastname: "McLenahan",
+      email: "emclenahang@samsung.com",
+      sex: "Female",
+      age: 54,
+      job: "Research Associate",
+    },
+    {
+      id: 18,
+      firstname: "Cory",
+      lastname: "Mayhew",
+      email: "cmayhewh@nyu.edu",
+      sex: "Male",
+      age: 71,
+      job: "VP Product Management",
+    },
+    {
+      id: 19,
+      firstname: "Neil",
+      lastname: "Marchington",
+      email: "nmarchingtoni@163.com",
+      sex: "Female",
+      age: 21,
+      job: "Senior Cost Accountant",
+    },
+    {
+      id: 20,
+      firstname: "Sidney",
+      lastname: "Wiltshire",
+      email: "swiltshirej@timesonline.co.uk",
+      sex: "Female",
+      age: 52,
+      job: "Tax Accountant",
+    },
+    {
+      id: 21,
+      firstname: "Kitti",
+      lastname: "Syfax",
+      email: "ksyfaxk@wunderground.com",
+      sex: "Female",
+      age: 61,
+      job: "Internal Auditor",
+    },
+    {
+      id: 22,
+      firstname: "Anatole",
+      lastname: "Goodfield",
+      email: "agoodfieldl@desdev.cn",
+      sex: "Male",
+      age: 56,
+      job: "Mechanical Systems Engineer",
+    },
+    {
+      id: 23,
+      firstname: "Kennith",
+      lastname: "Winridge",
+      email: "kwinridgem@nba.com",
+      sex: "Female",
+      age: 48,
+      job: "Safety Technician III",
+    },
+    {
+      id: 24,
+      firstname: "Gard",
+      lastname: "Klosa",
+      email: "gklosan@amazon.co.uk",
+      sex: "Female",
+      age: 56,
+      job: "GIS Technical Architect",
+    },
+    {
+      id: 25,
+      firstname: "Bethanne",
+      lastname: "Gyrgorcewicx",
+      email: "bgyrgorcewicxo@addtoany.com",
+      sex: "Male",
+      age: 53,
+      job: "Teacher",
+    },
+    {
+      id: 26,
+      firstname: "Yancey",
+      lastname: "Bilson",
+      email: "ybilsonp@washingtonpost.com",
+      sex: "Female",
+      age: 22,
+      job: "Financial Advisor",
+    },
+    {
+      id: 27,
+      firstname: "Gerrie",
+      lastname: "Borgesio",
+      email: "gborgesioq@uol.com.br",
+      sex: "Male",
+      age: 61,
+      job: "Food Chemist",
+    },
+    {
+      id: 28,
+      firstname: "Corny",
+      lastname: "Garnham",
+      email: "cgarnhamr@ed.gov",
+      sex: "Male",
+      age: 79,
+      job: "Assistant Professor",
+    },
+    {
+      id: 29,
+      firstname: "Hervey",
+      lastname: "Hackwell",
+      email: "hhackwells@jigsy.com",
+      sex: "Male",
+      age: 67,
+      job: "Web Designer I",
+    },
+    {
+      id: 30,
+      firstname: "Carey",
+      lastname: "Swindall",
+      email: "cswindallt@comsenz.com",
+      sex: "Female",
+      age: 81,
+      job: "Nuclear Power Engineer",
+    },
+  ];
+  // საშუალო ასაკი
+  let sum = 0;
+  let counter = 0;
+  for (var i = 0; i < data.length; i++ ){
+     sum += data[i].age
+     counter++;
+  }
+  average = sum / counter;
+  console.log("Average Age Is", average);
+ 
+ // 25-ზე ზევით
+ const namesarr = [ ]
+ for (var i = 0; i < data.length; i++){
+    if (data[i].age > 25) {
+       namesarr.push(data[i].firstname)
+    }
+ }
+ console.log(namesarr);
+ 
+ // იმეილი
+ const lastnamesarr = [ ]
+ for (var i = 0; i < data.length; i++){
+    if (data[i].email.endsWith ('.com')){
+       lastnamesarr.push(data[i].lastname);
+    }
+ }
+ console.log(lastnamesarr)
+ 
+ // სქესი
+ let male = 0;
+ let female = 0;
+ for (var i = 0; i < data.length; i++){
+    if (data[i].sex.includes ("Male")){
+       male++
+    }else if (data [i].sex.includes("Female")){
+       female++
+    }
+ }
+ console.log ("male :", male, "female :", female)
+ 
+ 
+ 
+  function averageAge(data) {
+     // იმპლემეტნაცია
+  }
+  
+  function moreThen25(data) {
+    // იმპლემეტნაცია
+  }
+  
+  function emailCom(data) {
+    // იმპლემეტნაცია
+  }
+  
+  function sexCount(data) {
+    // იმპლემეტნაცია
+  }
